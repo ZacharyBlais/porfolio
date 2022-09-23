e712913c87bfe5be0d9b2d79e511a3d35309bb8a
all0o <zacharyblais3@gmail.com>
2022-09-23 09:40:34 -0400

    avancement

--- inc/js/app.js
+++ inc/js/app.js
@@ -1,6 +1,10 @@
+let allButton = document.querySelector('.button');
 let button = document.querySelector('.clickMe');
-let button_bottom = document.querySelector('.bottom_rectangle');
+let button_bottomRectangle = document.querySelector('.bottom_rectangle');
+let button_bottomCircle = document.querySelector('.bottom_circle');
 let nameH1 = document.querySelector('.name');
+let nameContainer = document.querySelector('.name_container');
+let chargingBar = document.querySelector('.chargingBar');
 let devWebP = document.querySelector('.devWebP');
 let infoContainer = document.querySelector('.info_container');
 let socialBar = document.querySelector('.bar');
@@ -13,54 +17,69 @@ let mouseDown = false;
 
 let colorTable = ['082032', '150050', '041C32', '321F28', '54123B', '202040', 'C70D3A', '232931', 'FF9898', '28518A'];
 
-button.addEventListener('mousedown', () => {
+
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        nameContainer.classList.remove('w0');
+        button.classList.add('spawnButton');
+        allButton.classList.remove('opacity0')
+    }, 500);
+    setTimeout(() => {
+        button_bottomRectangle.classList.remove('opacity0');
+        button_bottomRectangle.classList.add('spawnBottomButton');
+        button_bottomCircle.classList.add('spawnBottomButtonNext')
+    }, 1500);
+    setTimeout(() => {
+        button.style.cursor = "pointer";
+        button.addEventListener('mousedown', pressed);
+    }, 2500)
+});
+    
+
+
+
+
+
+function pressed() {
     button.classList.add('pressed');
-    button_bottom.classList.add('bottom_pressed');
+    button_bottomRectangle.classList.add('bottom_pressed');
     clickCount++;
 
     if(clickCount <= 5) {
-        nameH1.style.transform = "scale("+ 0.2 * clickCount +", 0.5)";
+        chargingBar.style.transform = "scaleX("+ 0.2 * clickCount +")";
     }
 
-    if(clickCount == 6) {
-        devWebP.classList.add('devMove');
+    if(clickCount == 5) {
+        setTimeout(() => {
+            devWebP.classList.add("devMove")
+            nameH1.parentElement.style.border = "none";
+        }, 500)
         setTimeout(() => {
             nameH1.classList.add('appear')
-        }, 500);
+            chargingBar.style.transformOrigin = "right";
+            chargingBar.style.transform = "scaleX(0)";
+            
+        }, 1000);
+        setTimeout(() => {
+            infoContainer.classList.add('infoAppear');
+            socialIcones.forEach(item => {
+                item.classList.remove('opacity0');
+            })
+        }, 1300);
         
     }
 
-    if(clickCount == 7) {
-        infoContainer.classList.add('infoAppear');
-    }
+    if(clickCount > 5) {
 
-    if(clickCount == 8) {
-        let cooldown = 0;
-        if(window.innerWidth < 1000) {
-            cooldown = 1500;
-        }
-        socialBar.classList.add('socialMove');
-        setTimeout(() => {
-            socialIcones[0].classList.remove('displayNone');
-        }, 1500 - cooldown);
-        setTimeout(() => {
-            socialIcones[1].classList.remove('displayNone');
-        }, 1650 - cooldown);
-        setTimeout(() => {
-            socialIcones[2].classList.remove('displayNone');
-        }, 1800 - cooldown);
-    }
-
-    if(clickCount > 8) {
         body.style.background = "#"+colorTable[Math.floor(Math.random() * colorTable.length)];
     }
 
     console.log(clickCount)
-})
+}
 
 window.addEventListener('mouseup', () => {
     mouseDown = false;
     button.classList.remove('pressed');
-    button_bottom.classList.remove('bottom_pressed');
+    button_bottomRectangle.classList.remove('bottom_pressed');
 })
 
